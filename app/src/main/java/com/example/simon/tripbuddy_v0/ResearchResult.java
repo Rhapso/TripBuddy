0bee49fef8c5f27e8f4337422b98d7e59f68c268
simon <simon.fabry@telecom-paristech.fr>
2016-04-20 21:42:29 +0200

    Refactoring&Optimisation

--- app/src/main/java/com/example/simon/tripbuddy_v0/ResearchResult.java
+++ app/src/main/java/com/example/simon/tripbuddy_v0/ResearchResult.java
@@ -31,7 +31,7 @@ public class ResearchResult extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(new NavigationItemHandler(this));
     }
 
     @Override
@@ -44,27 +44,6 @@ public class ResearchResult extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.research_result, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
